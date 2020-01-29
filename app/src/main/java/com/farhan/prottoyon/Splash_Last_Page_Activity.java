bbeb59070ffec556608f3e768ac02819e7546933
Sakib Hossain <Sakibhossain321@gmail.com>
2020-01-29 15:02:58 +0600

    Merge branch 'master' of C:\Fiile\Project\New folder\Prottoyon with conflicts.

---
+++ app/src/main/java/com/farhan/prottoyon/Splash_Last_Page_Activity.java
@@ -0,0 +1,15 @@
+package com.farhan.prottoyon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Last_Page_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_last_page_acrivity);
+    }
+
+}
