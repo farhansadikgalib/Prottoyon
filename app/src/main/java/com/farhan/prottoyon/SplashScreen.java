68868e26343da82ffe490c2358791587af12bb4f
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2020-01-29 10:09:10 +0600

    SplashScreen is complete.

---
+++ app/src/main/java/com/farhan/prottoyon/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.farhan.prottoyon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    int SPLASH_TIME = 4000;
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        imageView = findViewById(R.id.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME);
+
+    }
+}
