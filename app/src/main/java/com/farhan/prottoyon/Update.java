35bad1ea4b3446c0cff01840e8072dc72599ba8b
Md  Shamim Hossain Sagor <43874324+sagor12345@users.noreply.github.com>
2020-02-24 12:06:09 +0600

    add Update Button

--- app/src/main/java/com/farhan/prottoyon/Update.java
+++ app/src/main/java/com/farhan/prottoyon/Update.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.farhan.prottoyon.ItemLayouts.i1_Activity_Approved_Application_details;
 
 public class Update extends AppCompatActivity {
+    Button update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        update = findViewById(R.id.Update);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Update.this,App_Des_with_CommentBox.class));
+
+            }
+        });
 
         ImageView backButton = findViewById(R.id.Shamprotic);
         backButton.setOnClickListener(new View.OnClickListener() {
