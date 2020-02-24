aac231a6507959ba7e23b448161101a2edd0f489
Md  Shamim Hossain Sagor <43874324+sagor12345@users.noreply.github.com>
2020-02-24 11:15:25 +0600

    add new Layout and all back button

--- app/src/main/java/com/farhan/prottoyon/App_Des_with_CommentBox.java
+++ app/src/main/java/com/farhan/prottoyon/App_Des_with_CommentBox.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.farhan.prottoyon.ItemLayouts.i1_Activity_Approved_Application_details;
 
 
 public class App_Des_with_CommentBox extends AppCompatActivity {
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_applicants_description_with_comment_box);
+
+
+        submit = findViewById(R.id.SubmitToUpdate);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(App_Des_with_CommentBox.this,Update.class));
+            }
+        });
         //Back Button Start
         ImageView backButton = findViewById(R.id.Shamprotic);
         backButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/farhan/prottoyon/Update.java
+++ app/src/main/java/com/farhan/prottoyon/Update.java
@@ -2,7 +2,12 @@ package com.farhan.prottoyon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.farhan.prottoyon.ItemLayouts.i1_Activity_Approved_Application_details;
 
 public class Update extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class Update extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        ImageView backButton = findViewById(R.id.Shamprotic);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(Update.this, App_Des_with_CommentBox.class));
+            }
+        });
     }
 }
