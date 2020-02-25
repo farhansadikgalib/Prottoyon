e127dca6d0d05950077d7b6a31e59a27b4fa4036
Sakib Hossain <Sakibhossain321@gmail.com>
2020-02-25 16:49:31 +0600

    fragment_applicants_description_with_comment_box xml updated

--- app/src/main/java/com/farhan/prottoyon/ApplicantsDescriptionWithCommentBox.java
+++ app/src/main/java/com/farhan/prottoyon/ApplicantsDescriptionWithCommentBox.java
@@ -1,6 +1,7 @@
 package com.farhan.prottoyon;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ApplicantsDescriptionWithCommentBox extends Fragment {
 
+    TextView NID;
+    Context context;
 
     public ApplicantsDescriptionWithCommentBox() {
         // Required empty public constructor
@@ -24,8 +26,22 @@ public class ApplicantsDescriptionWithCommentBox extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fragment_applicants_description_with_comment_box, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_applicants_description_with_comment_box, container, false);
+
+
+        return view;
+
+
+
+
+
+
+
+
     }
+
+
+
 
 }
